e1b6eeb3c26442d007ac6a9c96ab2fbe67323a48
md-isourov <mailtorogue@gmail.com>
2022-09-08 14:37:01 +0600

    first time publish try

---
+++ javascript/app.js
@@ -0,0 +1,39 @@
+// let hiThere = 94;
+// if (hiThere >= 90) {
+//     console.log('Well done')
+//     if (hiThere >= 95) {
+//         console.log('And Hell Yeah')
+//     } else {
+//         console.log('Regular')
+//     }
+// } else if (hiThere >= 80) {
+//     console.log('Not bad!')
+//     if (hiThere >= 88) {
+//         console.log('Needed Little Push')
+//     }
+// } else {
+//     console.log('Not so good')
+// }
+
+// let animal = 'dog'
+// let color = 'white'
+// if (animal === 'dog' || color === 'white') {
+//     console.log('Hell Ya')
+// } else {
+//     console.log('nah')
+// }
+
+// let age = 8
+// if (age <= 8 || age >= 70) {
+//     console.log(
+//         "no power")
+// } else {
+//     console.log('power')
+// }
+
+// let renNum = Math.random();
+// if (renNum > 0.5) {
+//     console.log("HEADS")
+// } else {
+//     console.log("TELLS")
+// }
